e9b51f508068c4965a1df55d7a3ef57b13f20dba
fsancheztemprano <fsancheztemprano@danielcastelao.org>
2019-10-25 11:37:11 +0200

    rename totalInOut

--- src/threads_06/Main06.java
+++ src/threads_06/Main06.java
@@ -9,34 +9,34 @@ public class Main06 extends Thread {
         int saldoInicial = 100;
         Caja caja = new Caja(saldoInicial);
 
-        int totalAudit = inOut(caja, 1000, 500);
+        int totalInOut = inOut(caja, 1000, 500);
 
         System.out.println(caja.toString());
-        System.out.println("Total requerido = " + totalAudit);
+        System.out.println("Total requerido = " + totalInOut);
 
     }
 
     private static int inOut(Caja caja, int ins, int outs) throws InterruptedException {
-        int totalAudit = caja.getSaldo();
+        int totalInOut = caja.getSaldo();
         ArrayList<Thread> threads = new ArrayList<>();
         for (int i = 0; i < outs; i++) {
             int out = new Random().nextInt(401) + 100;
             Consumer c = new Consumer(caja, out);
             c.start();
             threads.add(c);
-            totalAudit -= out;
+            totalInOut -= out;
         }
         for (int i = 0; i < ins; i++) {
             int in = new Random().nextInt(401) + 100;
             Producer p = new Producer(caja, in);
             p.start();
             threads.add(p);
-            totalAudit += in;
+            totalInOut += in;
         }
         for (Thread thread : threads) {
             thread.join();
         }
-        return totalAudit;
+        return totalInOut;
     }
 
 }
